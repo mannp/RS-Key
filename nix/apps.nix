7fcd2852759ba2ecc7bd5927e5c4ff7ce3d329e2
mannp <mannp@users.noreply.github.com>
2026-07-21 16:01:11 +0100

    Fix rsk-tui build.
    ┃        > Executing cargoSetupPostUnpackHook
    ┃        > Finished cargoSetupPostUnpackHook
    ┃        > Running phase: patchPhase
    ┃        > Executing cargoSetupPostPatchHook
    ┃        > Validating consistency between /build/source/Cargo.lock and /build/cargo-vendor-dir/Cargo.lock
    ┃        > Finished cargoSetupPostPatchHook
    ┃        > Running phase: updateAutotoolsGnuConfigScriptsPhase
    ┃        > Running phase: configurePhase
    ┃        > Running phase: buildPhase
    ┃        > Executing cargoBuildHook
    ┃        > cargoBuildHook flags: -j 4 --target x86_64-unknown-linux-gnu --offline --profile release
    ┃        > error: failed to load manifest for dependency `rsk-slip39`
    ┃        >
    ┃        > Caused by:
    ┃        >   failed to read `/crates/rsk-slip39/Cargo.toml`
    ┃        >
    ┃        > Caused by:
    ┃        >   No such file or directory (os error 2)
    ┃        For full logs, run:

--- nix/apps.nix
+++ nix/apps.nix
@@ -45,8 +45,8 @@ let
   rskTuiPkg = rustPlatform.buildRustPackage {
     pname = "rsk-tui";
     version = "0.1.0";
-    src = lib.cleanSource ../tools/tui;
-    cargoLock.lockFile = ../tools/tui/Cargo.lock;
+    src = lib.cleanSource ../..;
+    sourceRoot = "tools/tui";
     nativeBuildInputs = [ pkgs.pkg-config ];
     buildInputs = lib.optionals pkgs.stdenv.isLinux [
       pkgs.pcsclite
